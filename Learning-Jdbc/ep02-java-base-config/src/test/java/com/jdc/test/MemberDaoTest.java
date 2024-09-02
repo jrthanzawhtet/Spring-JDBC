a538f90c4375ee4bfc5ac9bd4736ea391045e716
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-09-02 20:45:17 +0630

    test

--- Learning-Jdbc/ep02-java-base-config/src/test/java/com/jdc/test/MemberDaoTest.java
+++ Learning-Jdbc/ep02-java-base-config/src/test/java/com/jdc/test/MemberDaoTest.java
@@ -5,10 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import com.jdc.jdbc.config.ApplicationConfig;
 import com.jdc.jdbc.dao.MemberDao;
 import com.jdc.jdbc.dto.Member;
 
-@SpringJUnitConfig(locations = "/application.xml")
+@SpringJUnitConfig(classes = ApplicationConfig.class)
 public class MemberDaoTest {
 	
 	@Autowired

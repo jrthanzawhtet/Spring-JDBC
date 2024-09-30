a65042f9ded7bf48b3e1b4e4fac2e1ba56fe4ba3
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-10-01 01:35:38 +0630

    generated pk

---
+++ Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dao/CategoryDao.java
@@ -0,0 +1,8 @@
+package com.jdc.spring.model.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryDao {
+
+}
---
+++ Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dto/Product.java
@@ -0,0 +1,5 @@
+package com.jdc.spring.model.dto;
+
+public class Product {
+
+}

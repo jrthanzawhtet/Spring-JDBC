d9c560b814c4389fe4730c1c7962c084ea862a9a
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-10-01 08:26:42 +0630

    jdbc test

--- Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dao/CategoryDao.java
+++ Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dao/CategoryDao.java
@@ -1,8 +1,31 @@
 package com.jdc.spring.model.dao;
 
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Service;
+
+import com.jdc.spring.model.dto.Category;
 
 @Service
 public class CategoryDao {
+	
+	@Autowired
+	private JdbcOperations jdbc;
+	
+	@Value("${dml.category.insert}")
+	private String insertSql;
+	
+	public int create(Category category) {
+		PreparedStatementCreator creator = conn -> {
+			var stmt = conn.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1,category.getName());
+			return stmt;
+		};
+		return 0;
+	}
 
 }

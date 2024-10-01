17648be496f6f39ee7ba3caee083c00138c41edf
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-10-01 07:31:03 +0630

    generated-id-for-hsqldb

--- Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dto/Category.java
+++ Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dto/Category.java
@@ -2,9 +2,11 @@ package com.jdc.spring.model.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Category {
 
 	private int id;
--- Learning-Jdbc/ep04-generated-pk/src/test/java/com/jdc/spring/test/CategoryDaoTest.java
+++ Learning-Jdbc/ep04-generated-pk/src/test/java/com/jdc/spring/test/CategoryDaoTest.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jdc.spring.model.dto.Category;
 
 @SpringJUnitConfig(locations = "/application.xml")
 @TestMethodOrder(OrderAnnotation.class)
@@ -19,6 +24,8 @@ public class CategoryDaoTest {
 	private JdbcOperations jdbc;
 	
 	@Test
+	@Order(1)
+	@Disabled
 	void test1() {
 		List<Object[]> params = new ArrayList<Object[]>();
 		params.add(new Object[] {1,"Fruid"});
@@ -30,6 +37,28 @@ public class CategoryDaoTest {
 
 		var count = jdbc.batchUpdate("INSERT INTO CATEGORY VALUES(?,?)", params);
 		Assertions.assertEquals(6, count.length);
+	}
+	
+	@Test
+	@Order(2)
+	void test2() {
+		List<Object[]> params = new ArrayList<Object[]>();
+		params.add(new Object[] {"Fruid"});
+		params.add(new Object[] {"Milk"});
+		params.add(new Object[] {"Electronic"});
+		params.add(new Object[] {"Kitchen"});
+		params.add(new Object[] {"Fancy"});
+		params.add(new Object[] {"Beauty"});
+
+		var count = jdbc.batchUpdate("INSERT INTO CATEGORY(name) VALUES(?)", params);
+		Assertions.assertEquals(6, count.length);
+	}
+	
+	@Test
+	@Order(3)
+	void test3() {
+		var list = jdbc.query("SELECT * FROM CATEGORY", new BeanPropertyRowMapper<Category>(Category.class));
+		System.out.println(list.toString());
 	}
 
 }

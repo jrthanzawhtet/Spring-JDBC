c6c72dd245afc8aa908fdbe162213731e67604cd
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-10-02 06:48:11 +0630

    Generated PK test

--- Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dao/CategoryDao.java
+++ Learning-Jdbc/ep04-generated-pk/src/main/java/com/jdc/spring/model/dao/CategoryDao.java
@@ -1,31 +1,49 @@
 package com.jdc.spring.model.dao;
 
 import java.sql.Statement;
+import java.sql.Types;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
 import org.springframework.stereotype.Service;
 
 import com.jdc.spring.model.dto.Category;
 
 @Service
 public class CategoryDao {
-	
+
 	@Autowired
 	private JdbcOperations jdbc;
-	
+
 	@Value("${dml.category.insert}")
 	private String insertSql;
-	
+
 	public int create(Category category) {
-		PreparedStatementCreator creator = conn -> {
-			var stmt = conn.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
-			stmt.setString(1,category.getName());
-			return stmt;
+		PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(insertSql, Types.VARCHAR);
+		factory.setReturnGeneratedKeys(true);
+
+		PreparedStatementCreator creator = factory.newPreparedStatementCreator(List.of(category.getName()));
+
+		PreparedStatementCallback<Integer> callback = stmt -> {
+			int affectedRows = stmt.executeUpdate();
+			if (affectedRows == 0) {
+				throw new IllegalStateException("Insert failed, no rows affected.");
+			}
+
+			var rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				return rs.getInt(1);
+			} else {
+				throw new IllegalStateException("Insert succeeded but no ID obtained.");
+			}
 		};
-		return 0;
+
+		return jdbc.execute(creator, callback);
 	}
 
 }
--- Learning-Jdbc/ep04-generated-pk/src/test/java/com/jdc/spring/test/CategoryDaoTest.java
+++ Learning-Jdbc/ep04-generated-pk/src/test/java/com/jdc/spring/test/CategoryDaoTest.java
@@ -14,51 +14,66 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import com.jdc.spring.model.dao.CategoryDao;
 import com.jdc.spring.model.dto.Category;
 
 @SpringJUnitConfig(locations = "/application.xml")
 @TestMethodOrder(OrderAnnotation.class)
 public class CategoryDaoTest {
-	
+
 	@Autowired
 	private JdbcOperations jdbc;
-	
+
+	@Autowired
+	private CategoryDao dao;
+
 	@Test
 	@Order(1)
 	@Disabled
 	void test1() {
 		List<Object[]> params = new ArrayList<Object[]>();
-		params.add(new Object[] {1,"Fruid"});
-		params.add(new Object[] {2,"Milk"});
-		params.add(new Object[] {3,"Electronic"});
-		params.add(new Object[] {4,"Kitchen"});
-		params.add(new Object[] {5,"Fancy"});
-		params.add(new Object[] {6,"Beauty"});
+		params.add(new Object[] { 1, "Fruid" });
+		params.add(new Object[] { 2, "Milk" });
+		params.add(new Object[] { 3, "Electronic" });
+		params.add(new Object[] { 4, "Kitchen" });
+		params.add(new Object[] { 5, "Fancy" });
+		params.add(new Object[] { 6, "Beauty" });
 
 		var count = jdbc.batchUpdate("INSERT INTO CATEGORY VALUES(?,?)", params);
 		Assertions.assertEquals(6, count.length);
 	}
-	
+
 	@Test
 	@Order(2)
+	@Disabled
 	void test2() {
 		List<Object[]> params = new ArrayList<Object[]>();
-		params.add(new Object[] {"Fruid"});
-		params.add(new Object[] {"Milk"});
-		params.add(new Object[] {"Electronic"});
-		params.add(new Object[] {"Kitchen"});
-		params.add(new Object[] {"Fancy"});
-		params.add(new Object[] {"Beauty"});
+		params.add(new Object[] { "Fruid" });
+		params.add(new Object[] { "Milk" });
+		params.add(new Object[] { "Electronic" });
+		params.add(new Object[] { "Kitchen" });
+		params.add(new Object[] { "Fancy" });
+		params.add(new Object[] { "Beauty" });
 
 		var count = jdbc.batchUpdate("INSERT INTO CATEGORY(name) VALUES(?)", params);
 		Assertions.assertEquals(6, count.length);
 	}
-	
+
 	@Test
 	@Order(3)
+	@Disabled
 	void test3() {
 		var list = jdbc.query("SELECT * FROM CATEGORY", new BeanPropertyRowMapper<Category>(Category.class));
 		System.out.println(list.toString());
+	}
+
+	@Test
+	@Order(4)
+	void test4() {
+		var c = new Category();
+		c.setName("Lemon");
+		var id = dao.create(c);
+		Assertions.assertEquals(1, id);
 	}
 
 }
